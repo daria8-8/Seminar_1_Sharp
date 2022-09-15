ec3bca0e206db5aa199ce4acc47dbcd0c9a832a6
Daria <daryaneronova@yandex.ru>
2022-09-15 22:04:20 +0300

    Ex002

---
+++ EX002/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Мы попали в игру угадай число");
+int secret_number = new Random().Next(1,11);
+int attempt = 5;
+bool Win = false;
+while (attempt > 0)
+{
+      Console.WriteLine("Введите число от 1 до 10");
+      int user_number = int.Parse(Console.ReadLine()); 
+      Console.WriteLine(user_number);   
+      if(user_number > secret_number)
+{
+    Console.WriteLine("Секретное число меньше");
+    attempt--;
+
+}
+else if(user_number < secret_number)
+{
+    Console.WriteLine("Секретное число больше");
+    attempt--;
+}
+else if(user_number == secret_number)
+{
+    Console.WriteLine("Мы угадали");
+    Win = true;
+    break;
+}
+Console.WriteLine("Попробуйте ещё раз, попыток осталось " + attempt);
+}
+if(Win)
+{
+    Console.WriteLine("Вы победили, увидимся через неделю");
+    Console.WriteLine("Осталось попыток " + (attempt-1));
+}else
+{
+    Console.WriteLine("Вы проиграли, секретное число было " + secret_number);
+    Console.WriteLine("Попробуйте ещё  раз");
+}
